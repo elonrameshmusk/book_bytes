829e8366f6651b8214c50bc58ecef15308d3f829
innovator ramesh <bvrameshhh@gmail.com>
2022-12-17 00:22:30 +0530

    added template layout with appbar and bottom navbar

---
+++ lib/views/components/colors.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+var colors = {
+  'primary': const Color(0xFF6750A4),
+  'on_primary': const Color(0xFFFFFFFF),
+  'primary_container': const Color(0xFFEADDFF),
+  'on_primary_container': const Color(0xFF21005D),
+  'secondary': const Color(0xFF625B71),
+  'on_secondary': const Color(0xFFFFFFFF),
+  'secondary_container': const Color(0xFFE8DEF8),
+  'on_secondary_container': const Color(0xFF1D192B),
+  'tertiary': const Color(0xFF7D5260),
+  'on_tertiary': const Color(0xFFFFFFFF),
+  'tertiary_container': const Color(0xFFFFD8E4),
+  'on_tertiary_container': const Color(0xFF31111D),
+  'error': const Color(0xFFB3261E),
+  'on_error': const Color(0xFFFFFFFF),
+  'error_container': const Color(0xFFF9DEDC),
+  'on_error_container': const Color(0xFF410E0B),
+  'background': const Color(0xFFFFFBFE),
+  'on_background': const Color(0xFF1C1B1F),
+  'surface': const Color(0xFFFFFBFE),
+  'on_surface': const Color(0xFF1C1B1F),
+  'outline': const Color(0xFF79747E),
+  'surface_variant': const Color(0xFFE7E0EC),
+  'on_surface_variant': const Color(0xFF49454F),
+
+};
---
+++ lib/views/screens/bytes_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class BytesScreen extends StatefulWidget {
+  const BytesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BytesScreen> createState() => _BytesScreenState();
+}
+
+class _BytesScreenState extends State<BytesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
