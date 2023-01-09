4e55f9e0fa9b604d942f1f2fe7e47be59e30b185
innovator ramesh <bvrameshhh@gmail.com>
2023-01-09 16:08:28 +0530

    made book list screen

--- lib/views/components/shelf_books_widget.dart
+++ lib/views/components/shelf_books_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bb/views/components/colors.dart';
+
 class ShelfBooksWidget extends StatefulWidget {
   const ShelfBooksWidget({Key? key}) : super(key: key);
 
@@ -8,20 +9,182 @@ class ShelfBooksWidget extends StatefulWidget {
 }
 
 class _ShelfBooksWidgetState extends State<ShelfBooksWidget> {
+  final Map<List<String>, bool> _books = {
+    [
+      'Elon Musk: How the billionaire CEO of SpaceX and Tesla is shaping our future',
+      'Ashlee Vance',
+      'https://m.media-amazon.com/images/I/51UUIOQ4qLL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+    ]: false,
+    [
+      'Steve Jobs',
+      'Walter Isaacson',
+      'https://m.media-amazon.com/images/I/41n1edvVlLL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+    ]: false,
+    [
+      'The man who knew infinity',
+      'Robert Kanigel',
+      'https://m.media-amazon.com/images/I/51iTh9JmZqL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+    ]: false,
+    [
+      'Einstein: his life and universe',
+      'Walter Isaacson',
+      'https://m.media-amazon.com/images/I/41ajKKgqY2L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+    ]: false,
+    [
+      'Edison: A life of invention',
+      'Paul Israel',
+      'https://m.media-amazon.com/images/I/41OyWx2uAIL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+    ]: false,
+    [
+      'The Google boys: Larry Page and Sergey Brin in their own words',
+      'Bagadi Venkat Rmaehs',
+      'https://m.media-amazon.com/images/I/51hSnBdwplL._SX373_BO1,204,203,200_.jpg'
+    ]: false,
+    [
+      'Bagadi Venkat Ramesh: His world we live in',
+      'Walter Isaacson',
+      'https://d2cbg94ubxgsnp.cloudfront.net/Pictures/480xAny/2/8/3/503283_the_universe_1.9781788686365.browse.0_22969.jpg'
+    ]: false,
+  };
+  bool item_select_mode = false;
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Page 2'),
-        ),
-        body: new Center(
-          child: new Text('PAGE 2'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
       ),
-      onWillPop: () async {
-        return false;
-      },
+      body: Container(
+        color: colors['background'],
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 7,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (item_select_mode) {
+                      return CheckboxListTile(
+                        title: Text(_books.keys.elementAt(index)[0]),
+                        value: _books[_books.keys.elementAt(index)],
+                        activeColor: Colors.pink,
+                        checkColor: Colors.white,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _books[_books.keys.elementAt(index)] = value!;
+                          });
+                        },
+                      );
+                    } else {
+                      return GestureDetector(
+                        onTap: () {
+                          print('book pressed');
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+                          height: 60,
+                          width: MediaQuery.of(context).size.width * 1,
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.20,
+                                height: MediaQuery.of(context).size.height * 1,
+                                child: Center(
+                                    child: Image(
+                                        image: NetworkImage(_books.keys.elementAt(index)[2])
+                                      // color: colors['primary_container'],
+                                    )),
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width * 0.60,
+                                  height: MediaQuery.of(context).size.height * 1,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        _books.keys.elementAt(index)[0],
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: colors['on_background']
+                                        ),
+                                      ),
+                                      Text(
+                                        _books.keys.elementAt(index)[1],
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: colors['on_surface_variant']
+                                        ),
+                                      ),
+                                    ],
+                                  )),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.20,
+                                height: MediaQuery.of(context).size.height * 1,
+                                child: Center(
+                                    child: Icon(
+                                      Icons.more_vert,
+                                      color: colors['outline'],
+                                    )),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                  }),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: colors['primary'],
+                    foregroundColor: colors['on_primary']
+                ),
+                child: const Text('Add new book'),
+                onPressed: () => showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    backgroundColor: colors['background'],
+                    title: Text('add new book',
+                        style: TextStyle(color: colors['on_background'])),
+                    content: TextField(
+                      // controller: _dialogTextFieldController,
+                      cursorColor: colors['primary'],
+                      decoration: InputDecoration(
+                        hintText: 'New book',
+                        contentPadding: const EdgeInsets.all(8),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: colors['primary']!, width: 2.0),
+                        ),
+                        border: const OutlineInputBorder(),
+                      ),
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(color: colors['primary']),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // print(_dialogTextFieldController.text);
+                          Navigator.pop(context, 'Add');
+                        },
+                        child: Text(
+                          'Add',
+                          style: TextStyle(color: colors['primary']),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
--- lib/views/components/shelves_widget.dart
+++ lib/views/components/shelves_widget.dart
@@ -38,7 +38,7 @@ class _ShelvesWidgetState extends State<ShelvesWidget> {
     _checkedItems.clear();
   }
 
-  bool itemSelectMode = false;
+  bool item_select_mode = false;
   final _dialogTextFieldController = TextEditingController();
   @override
   void dispose() {
@@ -48,166 +48,185 @@ class _ShelvesWidgetState extends State<ShelvesWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      onGenerateRoute: (settings){
-        Widget page = Scaffold(
-            appBar: AppBar(
-              scrolledUnderElevation: 0.0,
-              leading: IconButton(
-                onPressed: () {
-                  if (itemSelectMode) {
-                    setState(() {
-                      itemSelectMode = !itemSelectMode;
-                    });
-                  }
-                },
-                icon: Icon(itemSelectMode ? Icons.arrow_back : Icons.menu),
-              ),
-              title: Text(
-                itemSelectMode ? 'Select shelves' : 'Shelves',
-                style: TextStyle(color: colors['on_surface']),
-              ),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    if (itemSelectMode) {
-                      setState(() {
-                        _shelves.forEach((key, value) {
-                          _shelves[key] = true;
-                        });
-                      });
-                    } else {}
-                  },
-                  icon: Icon(itemSelectMode ? Icons.checklist : Icons.search),
-                ),
-                IconButton(
-                  onPressed: () {
-                    if (itemSelectMode) {
-                      getCheckboxItems();
-                    } else {
-                      setState(() {
-                        itemSelectMode = !itemSelectMode;
-                      });
-                    }
-                  },
-                  icon: Icon(
-                      itemSelectMode ? Icons.delete : Icons.check_box_outlined),
-                ),
-              ],
-            ),
-            body: Container(
-              color: colors['background'],
-              child: ListView.builder(
-                  itemCount: 15,
-                  itemBuilder: (BuildContext context, int index) {
-                    if (itemSelectMode) {
-                      return CheckboxListTile(
-                        title: Text(_shelves.keys.elementAt(index)),
-                        value: _shelves[_shelves.keys.elementAt(index)],
-                        activeColor: Colors.pink,
-                        checkColor: Colors.white,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _shelves[_shelves.keys.elementAt(index)] = value!;
-                          });
-                        },
-                      );
-                    } else {
-                      return GestureDetector(
-                        onTap: (){
-                          Navigator.pushNamed(context, 'shelf_books');
-                        },
-                        child: SizedBox(
-                          height: 60,
-                          width: MediaQuery.of(context).size.width * 1,
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.20,
-                                height: MediaQuery.of(context).size.height * 1,
-                                child: Center(
-                                    child: Icon(
-                                      Icons.folder,
-                                      color: colors['primary_container'],
-                                    )),
-                              ),
-                              SizedBox(
-                                  width: MediaQuery.of(context).size.width * 0.60,
-                                  height: MediaQuery.of(context).size.height * 1,
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        _shelves.keys.elementAt(index),
-                                      )
-                                    ],
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 0.0,
+        leading: IconButton(
+          onPressed: () {
+            if (item_select_mode) {
+              setState(() {
+                item_select_mode = false;
+              });
+            }
+          },
+          icon: Icon(item_select_mode ? Icons.arrow_back : Icons.menu),
+        ),
+        title: Text(
+          item_select_mode ? 'Select shelves' : 'Shelves',
+          style: TextStyle(color: colors['on_surface']),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (item_select_mode) {
+                setState(() {
+                  _shelves.forEach((key, value) {
+                    _shelves[key] = true;
+                  });
+                });
+              } else {}
+            },
+            icon: Icon(item_select_mode ? Icons.checklist : Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              if (item_select_mode) {
+                getCheckboxItems();
+              } else {
+                setState(() {
+                  item_select_mode = !item_select_mode;
+                });
+              }
+            },
+            icon: Icon(
+                item_select_mode ? Icons.delete : Icons.check_box_outlined),
+          ),
+        ],
+      ),
+      body: Container(
+          color: colors['background'],
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (item_select_mode) {
+                        return CheckboxListTile(
+                          title: Text(_shelves.keys.elementAt(index)),
+                          value: _shelves[_shelves.keys.elementAt(index)],
+                          activeColor: Colors.pink,
+                          checkColor: Colors.white,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _shelves[_shelves.keys.elementAt(index)] = value!;
+                            });
+                          },
+                        );
+                      } else {
+                        return GestureDetector(
+                          onTap: () {
+                            print('shelf pressed');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ShelfBooksWidget()),
+                            );
+                          },
+                          child: SizedBox(
+                            height: 60,
+                            width: MediaQuery.of(context).size.width * 1,
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.20,
+                                  height:
+                                      MediaQuery.of(context).size.height * 1,
+                                  child: Center(
+                                      child: Icon(
+                                    Icons.folder,
+                                    color: colors['primary_container'],
                                   )),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.20,
-                                height: MediaQuery.of(context).size.height * 1,
-                                child: Center(
-                                    child: Icon(
-                                      Icons.more_vert,
-                                      color: colors['outline'],
+                                ),
+                                SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.60,
+                                    height:
+                                        MediaQuery.of(context).size.height * 1,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          _shelves.keys.elementAt(index),
+                                        )
+                                      ],
                                     )),
-                              ),
-                            ],
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.20,
+                                  height:
+                                      MediaQuery.of(context).size.height * 1,
+                                  child: Center(
+                                      child: Icon(
+                                    Icons.more_vert,
+                                    color: colors['outline'],
+                                  )),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                    }),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: colors['primary'],
+                    foregroundColor: colors['on_primary']
+                  ),
+                  child: const Text('Add new shelf'),
+                  onPressed: () => showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      backgroundColor: colors['background'],
+                      title: Text('Create new shelf',
+                          style: TextStyle(color: colors['on_background'])),
+                      content: TextField(
+                        controller: _dialogTextFieldController,
+                        cursorColor: colors['primary'],
+                        decoration: InputDecoration(
+                          hintText: 'New shelf',
+                          contentPadding: const EdgeInsets.all(8),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: colors['primary']!, width: 2.0),
+                          ),
+                          border: const OutlineInputBorder(),
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'Cancel'),
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(color: colors['primary']),
                           ),
                         ),
-                      );
-                    }
-                  }),
-            ),
-            floatingActionButton: Visibility(
-              visible: !itemSelectMode,
-              child: FloatingActionButton(
-                onPressed: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    backgroundColor: colors['background'],
-                    title: Text('Create new shelf',
-                        style: TextStyle(color: colors['on_background'])),
-                    content: TextField(
-                      controller: _dialogTextFieldController,
-                      cursorColor: colors['primary'],
-                      decoration: InputDecoration(
-                        hintText: 'New folder',
-                        contentPadding: const EdgeInsets.all(8),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                          BorderSide(color: colors['primary']!, width: 2.0),
+                        TextButton(
+                          onPressed: () {
+                            print(_dialogTextFieldController.text);
+                            Navigator.pop(context, 'Create');
+                          },
+                          child: Text(
+                            'Create',
+                            style: TextStyle(color: colors['primary']),
+                          ),
                         ),
-                        border: const OutlineInputBorder(),
-                      ),
+                      ],
                     ),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () => Navigator.pop(context, 'Cancel'),
-                        child: Text(
-                          'Cancel',
-                          style: TextStyle(color: colors['primary']),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          print(_dialogTextFieldController.text);
-                          Navigator.pop(context, 'Create');
-                        },
-                        child: Text(
-                          'Create',
-                          style: TextStyle(color: colors['primary']),
-                        ),
-                      ),
-                    ],
                   ),
                 ),
-                child: const Icon(Icons.add),
-              ),
-            ),);
-        if(settings.name=='shelf_books') page = ShelfBooksWidget();
-        return MaterialPageRoute(builder: (_)=>page);
-      },
+              )
+            ],
+          )),
+
     );
   }
 }
